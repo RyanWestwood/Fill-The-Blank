095fbc581fbed2bce677f147f81c65b3b5bda343
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-10-21 20:50:01 +0000

    Clamped scroll

--- Fill-The-Blank/Assets/Resolution.cs
+++ Fill-The-Blank/Assets/Resolution.cs
@@ -8,11 +8,17 @@ public class Resolution : MonoBehaviour
     public RectTransform m_Screen;
     public RectTransform m_Shop;
     public RectTransform m_Character;
+    public RectTransform m_Menu;
     void Start()
     {
-        Debug.Log("Helo\n");
         float w = m_Screen.rect.width;
         m_Shop.anchoredPosition = new Vector2(w, 0);
         m_Character.anchoredPosition = new Vector2(-w, 0);
+    }
+
+    public void Clamp()
+    {
+        float pos = Mathf.Clamp(m_Menu.anchoredPosition.x, -m_Screen.rect.width, m_Screen.rect.width);
+        m_Menu.anchoredPosition = new Vector2(pos, 0);
     }
 }
