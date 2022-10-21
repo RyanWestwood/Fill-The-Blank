9c3fdc29d8a4ad4017cf50709e36a1310fc5f1af
Ryan <55410510+RyanWestwood@users.noreply.github.com>
2022-10-21 14:03:38 +0100

    Added clicks

---
+++ Fill-The-Blank/Assets/Button.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public void AvatarClick()
+    {
+        Debug.Log("Avatar Opened!\n");
+    }
+
+    public void CurrencyClick()
+    {
+        Debug.Log("Currency Opened!\n");
+    }
+
+    public void BattlePassClick()
+    {
+        Debug.Log("Battle Pass Opened!\n");
+    }
+
+    public void PlayClick()
+    {
+        Debug.Log("Play Game!\n");
+    }
+
+    public void ZenClick()
+    {
+        Debug.Log("Zen Game!\n");
+    }
+
+    public void ShopClick()
+    {
+        Debug.Log("Shop Opened!\n");
+    }
+
+    public void GameClick()
+    {
+        Debug.Log("Game Opened!\n");
+    }
+
+    public void CharacterClick()
+    {
+        Debug.Log("Character Opened!\n");
+    }
+}
